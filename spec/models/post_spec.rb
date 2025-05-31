1e47796c61d13a72fb343875a59609c3cae4d212
VictorHSCosta <victorhenriquecosta23@gmail.com>
2025-05-31 12:20:46 -0300

    Add PostsHelper and corresponding specs; implement request specs for Posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,17 @@ RSpec.describe Post, type: :model do
   context 'before publication' do
     it 'when title is nil' do
       post = Post.create(description: "Description valid")
+      expect(post).not_to be_valid
+    end
 
+    it 'when description is nil' do
+      post = Post.create(title: "Title valid")
+      expect(post).not_to be_valid
+    end
 
+    it "when post is valid" do
+      post = Post.create(title: "Title valid", description: "Description valid")
+      expect(post).to be_valid
+    end
   end
 end

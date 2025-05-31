b25902568ddf471a905bd2a426ed287674db14ac
VictorHSCosta <victorhenriquecosta23@gmail.com>
2025-05-31 17:14:17 -0300

    feat: add PostCSS configuration with Tailwind CSS and Autoprefixer
    - Created postcss.config.js to include Tailwind CSS and Autoprefixer as plugins.
    - Added tailwind.config.js with custom font family and primary color configuration.
    - Updated tsconfig.app.json to set baseUrl and paths for easier imports in the project.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['IBM Plex Sans', 'sans-serif'],
+      },
+      colors: {
+        primary: '#1D4ED8',
+      }
+    },
+  },
+  plugins: [],
+}
+
